c8e317d586464fda70454b7d5e3dd04a9e28a600
Daniel Martin Jimenez <danmarjim@gmail.com>
2017-04-17 16:52:36 +0200

    Finish

--- newsfeed/Models/Utils/Style.swift
+++ newsfeed/Models/Utils/Style.swift
@@ -16,7 +16,7 @@ struct Style {
     static var secondColor = UIColor(red: 0/255, green: 168/255, blue: 126/255, alpha: 1.0)
     static var thirdColor = UIColor(red: 252/255, green: 91/255, blue: 32/255, alpha: 1.0)
     
-    //Color with Alpha
+    //Colors with Alpha
     static var thirdColorWithAlpha = UIColor(red: 252/255, green: 91/255, blue: 32/255, alpha: 0.6)
     
     //Fonts NFLaunch
@@ -24,10 +24,16 @@ struct Style {
     
     //Fonts NFLogin
     static var fontTitleLogin = UIFont (name: "Rondalo", size: 50)
-    static var fontNavBar = UIFont (name: "Rondalo", size: 25)
     static var fontPlaceholderLogin = UIFont (name: "OCRB10PitchBT-Regular", size: 16)
     static var fontTextButtonLogin = UIFont (name: "OCRB10PitchBT-Regular", size: 18)
     
+    //Fonts NFFeedList
+    static var fontFeedList = UIFont (name: "Rondalo", size: 50)
+    
+    //Fonts NFInfoFeed
+    static var fontTitleInfoFeed = UIFont (name: "Rondalo", size: 50)
+    static var fontTextInfoFeed = UIFont (name: "OCRB10PitchBT-Regular", size: 16)
+    
     //Fonts NavBar
-    static var fontTextNavBar = UIFont (name: "OCRB10PitchBT-Regular", size: 24)
+    static var fontNavBar = UIFont (name: "Rondalo", size: 25)
 }
