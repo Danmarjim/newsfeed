43dd9ecaf37113cab43623e1ce3b1ce4ae5a0efe
Daniel Martin Jimenez <daniel.martin.jimenez@everis.com>
2018-06-03 16:15:06 +0200

    Reorganized project.

--- newsfeed/Utils/Style.swift
+++ newsfeed/Utils/Style.swift
@@ -16,7 +16,21 @@ struct Style {
     static var secondColor = UIColor(red: 0/255, green: 168/255, blue: 126/255, alpha: 1.0)
     static var thirdColor = UIColor(red: 252/255, green: 91/255, blue: 32/255, alpha: 1.0)
     
+    //Fonts NFLaunch
+    static var fontTitleLaunch = UIFont (name: "Rondalo", size: 60)
+    
     //Fonts NFLogin
-    static var fontTitleLogin = UIFont (name: "Avenir-Light", size: 50)
-    static var fontPlaceholderLogin = UIFont (name: "Avenir-Bold", size: 14)
+    static var fontTitleLogin = UIFont (name: "Rondalo", size: 50)
+    static var fontPlaceholderLogin = UIFont (name: "OCRB10PitchBT-Regular", size: 16)
+    static var fontTextButtonLogin = UIFont (name: "OCRB10PitchBT-Regular", size: 18)
+    
+    //Fonts NFFeedList
+    static var fontFeedList = UIFont (name: "Rondalo", size: 25)
+    
+    //Fonts NFInfoFeed
+    static var fontTitleInfoFeed = UIFont (name: "Rondalo", size: 26)
+    static var fontTextInfoFeed = UIFont (name: "OCRB10PitchBT-Regular", size: 20)
+    
+    //Fonts NavBar
+    static var fontNavBar = UIFont (name: "Rondalo", size: 24)
 }
